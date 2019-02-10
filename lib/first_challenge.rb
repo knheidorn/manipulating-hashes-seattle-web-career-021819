fd8715148eb419326d248134e6c3db9e08033ccc
Kim Heidorn <heidorkn@gmail.com>
2019-02-10 20:19:59 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,7 +13,7 @@ def first_challenge
     }
   }
 
-  contacts["Freddy Mercury"][favorite_icecream_flavors].delete_if
+  contacts["Freddy Mercury"][favorite_icecream_flavors].delete_if do |
 
 
   #remember to return your newly altered contacts hash!
